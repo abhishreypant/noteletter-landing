23ef283231a339c656e2804705e691c17e74d666
abhishreypant <59860238+abhishreypant@users.noreply.github.com>
2020-12-16 12:48:14 +0530

    fixed changes

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
         </nav>
         <section class='hero-heading w-10/12 flex-1 pt-8 md:w-8/12 md:pt-24 mx-auto flex-col flex md:flex-row md:justify-between md:items-center'>
           <div class='text order-2 md:order-1'>
-            <h1 class='text-6xl text-center leading-none font-light text-brand-600 tracking-tighter md:text-7xl md:text-left md:leading-none md:font-medium font-albra'>Build a tribe <br/>which loves your <br/> bookmarks.</h1>
+            <h1 class='text-6xl text-center leading-none font-light text-brand-600 tracking-tighter md:text-7xl md:text-left md:leading-none md:font-medium font-playfair'>Build a tribe <br/>which loves your <br/> bookmarks.</h1>
 
             <p class='my-6 md:my-12 text-lg md:text-xl tracking-tight text-brand-600 font-medium text-center md:text-left'>
               <span class='md:block'>Pack Your Bookmarks Into A Powerful & Value Packed E-Mail</span>
@@ -65,13 +65,13 @@ function App() {
 
         <div class='w-10/12 mx-auto md:w-8/12 md:py-0 flex items-center justify-around text-center md:text-left py-12 md:my-24 md:justify-start'>
           <div class='mr-2 md:mr-10'>
-            <h2 class='text-4xl md:text-6xl tracking-tighter font-albra font-thin text-brand-600'>100x+</h2>
-            <h4 class='text-md mt-2 md:mt-4 font-albra tracking-tighter text-brand-600 text-base'>Signups Every Week</h4>
+            <h2 class='text-4xl md:text-6xl tracking-tighter font-playfair font-thin text-brand-600'>100x+</h2>
+            <h4 class='text-md mt-2 md:mt-4 font-playfair tracking-tighter text-brand-600 text-base'>Signups Every Week</h4>
           </div>
 
           <div class='mr-2 md:mr-10'>
-            <h2 class='text-4xl md:text-6xl tracking-tighter font-albra font-thin text-brand-600'>65+</h2>
-            <h4 class='text-md mt-2 md:mt-4 font-albra tracking-tighter text-brand-600 text-base'>Premium Members</h4>
+            <h2 class='text-4xl md:text-6xl tracking-tighter font-playfair font-thin text-brand-600'>65+</h2>
+            <h4 class='text-md mt-2 md:mt-4 font-playfair tracking-tighter text-brand-600 text-base'>Premium Members</h4>
           </div>
         </div>
       </section>
@@ -83,36 +83,41 @@ function App() {
 
           <div class='w-11/12 my-6 text-center md:text-left mx-auto md:bg-gray-800 md:mt-16 md:p-16 text-white flex flex-col justify-start items-center md:flex-row md:justify-between md:py-16 md:px-24'>
             <div>
-              <h4 class='text-lg md:text-2xl capitalize'>
-                <i class="lni lni-layers text-4xl my-4"></i><br/>
-                collect your <br/> 
-                valuable bookmarks <br/> 
-                into the noteletter<br/> 
-                dashboard</h4>
+               <h4 class='text-lg md:text-2xl capitalize'>
+                  <i class="lni lni-layers text-4xl my-4"></i><br/>
+                  <span class='md:block'>collect your </span>
+                  <span class='md:block'>valuable bookmarks </span>
+                  <span class='md:block'>into the noteletter </span>
+                  <span class='md:block'>dashboard. </span>
+                </h4>
             </div>
             <img src={ss1} class='md:w-2/4 shadow-xl md:my-0 block origin-right w-full my-6'/>
           </div>
 
           <div class='w-11/12 my-6 text-center md:text-left mx-auto md:bg-gray-800 md:mt-16 md:p-16 text-white flex flex-col justify-start items-center md:flex-row md:justify-between md:py-16 md:px-24'>
             <div class='flex-1'>
-              <h4 class='text-lg md:text-2xl capitalize'>
-                <i class="lni lni-checkmark-circle text-4xl my-4"></i><br/>
-                collect your <br/> 
-                valuable bookmarks <br/> 
-                into the noteletter<br/> 
-                dashboard</h4>
+                <h4 class='text-lg md:text-2xl capitalize'>
+                  <i class="lni lni-checkmark-circle text-4xl my-4"></i><br/>
+                  <span class='md:block'>Select Your   </span>
+                  <span class='md:block'>respective week’s  </span>
+                  <span class='md:block'>bookmarks you want </span>
+                  <span class='md:block'>to send to your</span>
+                  <span class='md:block'> subscirbers</span>
+                </h4>
             </div>
             <img src={ss2} class='md:w-2/4 shadow-xl md:my-0 block origin-right w-full my-6 scale-300'/>
           </div>
 
           <div class='w-11/12 my-6 text-center md:text-left mx-auto md:bg-gray-800 md:mt-16 md:p-16 text-white flex flex-col justify-start items-center md:flex-row md:justify-between md:py-16 md:px-24'>
             <div class='flex-1'>
-              <h4 class='text-lg md:text-2xl capitalize'>
-                <i class="lni lni-chevron-right-circle text-4xl my-4"></i><br/>
-                collect your <br/> 
-                valuable bookmarks <br/> 
-                into the noteletter<br/> 
-                dashboard</h4>
+                <h4 class='text-lg md:text-2xl capitalize'>
+                  <i class="lni lni-chevron-right-circle text-4xl my-4"></i><br/>
+                  <span class='md:block'>Curate An </span>
+                  <span class='md:block'>Awesome Email, </span>
+                  <span class='md:block'>send it to your followers, </span>
+                  <span class='md:block'>Watch Your Followers</span>
+                  <span class='md:block'>Grow !</span>
+                </h4>
             </div>
             <img src={ss3} class='md:w-2/4 shadow-xl md:my-0 block origin-right w-full my-6'/>
           </div>
@@ -124,28 +129,28 @@ function App() {
           <div class='flex justify-end'>
             <i class="lni lni-cross-circle w-12 flex items-center justify-center h-12 text-brand-600 rounded-full text-2xl md:text-3xl md:mb-0 mb-6"></i>
           </div>
-          <h1 class='text-3xl md:text-5xl font-albra text-brand-600 text-center capitalize md:leading-snug'>you are 119 of 400 in <span class='md:block'>the line. 🎉</span></h1>
+          <h1 class='text-3xl md:text-5xl font-playfair text-brand-600 text-center capitalize md:leading-snug'>you are 119 of 400 in <span class='md:block'>the line. 🎉</span></h1>
           <form class='flex flex-col md:flex-row justify-center items-center my-6 md:my-12'>
-            <input type='text' disabled value='https://www.noteletter.io/referall?=48' class='w-full md:w-8/12 p-4'/>
+            <input type='text' disabled value='https://www.noteletter.io/referall?=48' class='w-full md:w-8/12 p-4 bg-gray-100'/>
             <input type='text' disabled value='Copy Code' class='w-full md:w-4/12 p-4 bg-brand-500 text-white text-center'/>
           </form>
         </div>
 
-        <div class='py-14 md:py-32 w-10/12 md:w-8/12 mx-auto'>
-          <h1 class='text-3xl md:text-5xl font-albra text-brand-600 text-center'>Join The Exclusive Tribe Of Beta <span class='md:block'>Testers Of Noteletter.</span></h1>
+        <div class='py-14 md:py-32 w-10/12 md:w-6/12 mx-auto'>
+          <h1 class='text-3xl md:text-5xl font-playfair text-brand-600 text-center'>Join The Exclusive Tribe Of Beta <span class='md:block'>Testers Of Noteletter.</span></h1>
           <img src={beta} class='my-8 md:my-24 block mx-auto w-10/12 md:w-5/12'></img>
-          <form class='flex flex-col md:flex-row items-center md:w-8/12 mx-auto justify-center'>
-            <input class='w-full md:w-8/12 text-lg inline-block text-center md:text-left p-4 md:py-7 md:px-12 my-1 md:my-0 md:mx-1 bg-white' type='text' placeholder='example@gmail.com'></input>
-            <input class='w-full md:w-4/12 inline-block p-4 md:py-7 md:px-12 my-1 md:my-0 md:mx-1 bg-brand-500 text-white text-lg' value='Join the Tribe' type='submit'></input>
+          <form class='flex flex-col md:flex-row items-center mx-auto justify-center'>
+            <input class='w-full md:w-8/12 px-6 py-3 md:py-6 md:px-12 bg-white rounded-sm m-1.5 cursor-pointer hover:opacity-90' type='text' placeholder='example@gmail.com'></input>
+            <input class='w-full md:w-4/12 px-6 py-3 md:py-6 md:px-12 bg-brand-500 text-white rounded-sm m-1.5 cursor-pointer hover:opacity-90' value='Join the Tribe' type='submit'></input>
           </form>
         </div>
       </section>
 
       <section class='bg-brand-100'>
-        <div class='py-14 md:py-32 w-10/12 md:w-8/12 mx-auto flex flex-col md:flex-row items-center'>
+        <div class='py-14 md:py-32 w-10/12 md:w-6/12 mx-auto flex flex-col md:flex-row items-center'>
           <div class='flex-2 md:w-3/4'>
-            <h4 class='font-albra text-4xl md:text-5xl  text-center  text-brand-600 tracking-tighter md:tracking-tight md:text-left'>Don't want to start a <br/> newsletter? </h4>
-            <h6 class='text-lg md:text-xl text-center font-medium text-brand-600 md:mt-6 tracking-tight md:text-left md:mb-0 mt-4 mb-4'>Don't worry. Use noteletter like <br/> thousands of others to save <br/> bookmarks for later</h6>
+            <h4 class='font-playfair text-4xl md:text-5xl  text-center  text-brand-600 tracking-tighter md:tracking-tight md:text-left'>Don't want to start a <br/> newsletter? </h4>
+            <h6 class='text-lg md:text-xl text-center font-regular text-brand-600 md:mt-6 tracking-normal md:text-left md:mb-0 mt-4 mb-4'>Don't worry. Use noteletter like <br/> thousands of others to save <br/> bookmarks for later</h6>
           </div>
           <div class='flex-1 md:w-1/4 flex justify-center md:justify-end'>
             <img src={reminder}/>
@@ -159,7 +164,7 @@ function App() {
           <img src={logo}/>
         </div>
         <div class='py-14 md:py-24 w-10/12 md:w-8/12 mx-auto'>
-            <h4 class='font-albra text-4xl font-medium md:leading-tight md:text-5xl text-center text-brand-600 tracking-tighter md:tracking-tight capitalize '>Get started with <br/> Noteletter for free now</h4>
+            <h4 class='font-playfair text-4xl font-medium md:leading-tight md:text-5xl text-center text-brand-600 tracking-tighter md:tracking-tight capitalize '>Get started with <br/> Noteletter for free now</h4>
             <form class='flex flex-col md:flex-row md:justify-center my-12'>
               <input type='text' placeholder='example@gmail.com' class='md:mx-1 text-center px-6 py-2.5 bg-white text-brand-600 cursor-pointer hover:opacity-90'></input>
               <input type='submit' value='Create A free Acount Now' class='md:mx-1 px-6 py-2.5 bg-brand-500 text-white cursor-pointer hover:opacity-90'></input>
